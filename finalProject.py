e3b59a2e2cd3e8a80d23880a1bbae114837a9834
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-06-06 22:50:29 -0700

    Add restaurantMenuJSON method

--- finalProject.py
+++ finalProject.py
@@ -13,6 +13,12 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
 
 #Show all restaurants
 @app.route('/')
